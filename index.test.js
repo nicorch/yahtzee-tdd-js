52fa9b3b18cbc4e0603adcc5e75e14a04370cc3e
NicolasRoche <nicolas8roche@gmail.com>
2021-10-08 10:14:21 +0200

    :white_check_mark: Ones three throw tests Ok

--- index.test.js
+++ index.test.js
@@ -9,6 +9,14 @@ describe('Scoring Ones', () => {
         const result = yahtzee.score('Ones', [1,2,3,4,5])      
         assert.equal(result, 1)
     })
+    it(`should return 2 when scoring [1,1,3,4,5] for Ones`, () => {      
+        const result = yahtzee.score('Ones', [1,1,3,4,5])      
+        assert.equal(result, 2)
+    })
+    it(`should return 3 when scoring [1,1,1,4,5] for Ones`, () => {      
+        const result = yahtzee.score('Ones', [1,1,1,4,5])      
+        assert.equal(result, 3)
+    })
 })
 
 // Test Twos
