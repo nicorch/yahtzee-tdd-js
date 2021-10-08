e25bda2e7e2fce568568c54a51a73209b32ff35a
NicolasRoche <nicolas8roche@gmail.com>
2021-10-08 10:30:31 +0200

    :white_check_mark: :bug: edit tests mistakes

--- index.test.js
+++ index.test.js
@@ -20,18 +20,18 @@ describe('Scoring Ones', () => {
 })
 
 // Test Twos
-describe('Scoring Ones', () => {
-    it(`should return 1 when scoring [1,2,3,4,5] for Twos`, () => {      
+describe('Scoring Twos', () => {
+    it(`should return 2 when scoring [1,2,3,4,5] for Twos`, () => {      
         const result = yahtzee.score('Twos', [1,2,3,4,5])      
-        assert.equal(result, 1)
+        assert.equal(result, 2)
     })
     it(`should return 0 when scoring [1,1,3,4,5] for Twos`, () => {      
         const result = yahtzee.score('Twos', [1,1,3,4,5])      
         assert.equal(result, 0)
     })
-    it(`should return 4 when scoring [2,2,2,4,2] for Twos`, () => {      
+    it(`should return 8 when scoring [2,2,2,4,2] for Twos`, () => {      
         const result = yahtzee.score('Twos', [2,2,2,4,2])      
-        assert.equal(result, 4)
+        assert.equal(result, 8)
     })
 })
 
